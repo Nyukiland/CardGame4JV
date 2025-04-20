b42c60bfafbb1ab4d46f4084e63cf856babb53b9
Nyukiland <roman211113@gmail.com>
2025-04-20 19:39:24 +0200

    [FIX] SubClassSelector - fixed visual

--- Assets/Tool/Attribute/Editor/SubclassSelectorAttributeDrawer.cs
+++ Assets/Tool/Attribute/Editor/SubclassSelectorAttributeDrawer.cs
@@ -2,10 +2,16 @@ using System;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using System.Collections.Generic;
+
+//based on Mackysoft's work
+//code : https://github.com/mackysoft/Unity-SerializeReferenceExtensions/blob/main/Assets/MackySoft/MackySoft.SerializeReferenceExtensions/Editor/SubclassSelectorDrawer.cs
 
 [CustomPropertyDrawer(typeof(SubclassSelectorAttribute))]
 public class SubclassSelectorAttributeDrawer : PropertyDrawer
 {
+	private static readonly Dictionary<Type, Type[]> DerivedTypesCache = new();
+
 	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
 	{
 		EditorGUI.BeginProperty(position, label, property);
@@ -18,17 +24,31 @@ public class SubclassSelectorAttributeDrawer : PropertyDrawer
 		}
 
 		var attr = (SubclassSelectorAttribute)attribute;
-		var baseType = attr.BaseType;
-		var derivedTypes = AppDomain.CurrentDomain.GetAssemblies()
-			.SelectMany(a => a.GetTypes())
-			.Where(t => baseType.IsAssignableFrom(t) && !t.IsAbstract && !t.IsInterface)
-			.ToArray();
+		Type baseType = attr.BaseType;
 
-		string[] options = derivedTypes.Select(t => t.FullName).ToArray();
+		// Get or cache derived types
+		if (!DerivedTypesCache.TryGetValue(baseType, out Type[] derivedTypes))
+		{
+			derivedTypes = AppDomain.CurrentDomain.GetAssemblies()
+				.SelectMany(assembly => assembly.GetTypes())
+				.Where(t => baseType.IsAssignableFrom(t) && !t.IsAbstract && !t.IsInterface)
+				.ToArray();
+
+			DerivedTypesCache[baseType] = derivedTypes;
+		}
+
+		// Draw foldout
+		Rect foldoutRect = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
+		property.isExpanded = EditorGUI.Foldout(foldoutRect, property.isExpanded, label, true);
+		position.y += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+
+		// Draw type selector popup
 		int currentIndex = Array.FindIndex(derivedTypes, t => t == property.managedReferenceValue?.GetType());
+		string[] options = derivedTypes.Select(t => t.FullName).ToArray();
 
-		Rect dropdownRect = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
-		int selected = EditorGUI.Popup(dropdownRect, label.text, currentIndex, options);
+		Rect popupRect = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
+		int selected = EditorGUI.Popup(popupRect, "Type", currentIndex, options);
+		position.y += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
 
 		if (selected != currentIndex && selected >= 0)
 		{
@@ -37,19 +57,23 @@ public class SubclassSelectorAttributeDrawer : PropertyDrawer
 			property.isExpanded = true;
 		}
 
+		// Draw child properties
 		if (property.isExpanded && property.managedReferenceValue != null)
 		{
 			EditorGUI.indentLevel++;
 			SerializedProperty iterator = property.Copy();
-			var end = iterator.GetEndProperty();
+			SerializedProperty end = iterator.GetEndProperty();
+
 			bool enterChildren = true;
 			while (iterator.NextVisible(enterChildren) && !SerializedProperty.EqualContents(iterator, end))
 			{
-				position.y += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
-				Rect propRect = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
-				EditorGUI.PropertyField(propRect, iterator, true);
+				float childHeight = EditorGUI.GetPropertyHeight(iterator, true);
+				Rect childRect = new Rect(position.x, position.y, position.width, childHeight);
+				EditorGUI.PropertyField(childRect, iterator, true);
+				position.y += childHeight + EditorGUIUtility.standardVerticalSpacing;
 				enterChildren = false;
 			}
+
 			EditorGUI.indentLevel--;
 		}
 
@@ -58,12 +82,13 @@ public class SubclassSelectorAttributeDrawer : PropertyDrawer
 
 	public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
 	{
-		float height = EditorGUIUtility.singleLineHeight;
+		float height = EditorGUIUtility.singleLineHeight * 2 + EditorGUIUtility.standardVerticalSpacing * 2;
 
 		if (property.isExpanded && property.managedReferenceValue != null)
 		{
 			SerializedProperty iterator = property.Copy();
-			var end = iterator.GetEndProperty();
+			SerializedProperty end = iterator.GetEndProperty();
+
 			bool enterChildren = true;
 			while (iterator.NextVisible(enterChildren) && !SerializedProperty.EqualContents(iterator, end))
 			{
