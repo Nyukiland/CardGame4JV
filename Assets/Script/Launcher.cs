23c47e9f4edd217d3b9a64682477fd68bfb94418
Blanco <96238298+BlancoTLM@users.noreply.github.com>
2025-06-13 12:08:12 +0200

    [UPD] Hide the event system when loading scene in additive

--- Assets/Script/Launcher.cs
+++ Assets/Script/Launcher.cs
@@ -1,17 +1,40 @@
 using System.Collections.Generic;
 using CardGame.Useful;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.ResourceManagement;
-using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.SceneManagement;
 
 namespace CardGame
 {
+    // Script used to initalize the game, might be moved later
     public class Launcher : MonoBehaviour
     {
-        // Script used to initalize the game, might be moved later
+        [SerializeField] private EventSystem _eventSystem;
+        public static Launcher Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            Instance = this;
+        }
+        
         void Start()
         {
             ResourceManager.ExceptionHandler = AddressableErrorHandler.HandleAddressableException;
+            
+            if (SceneManager.loadedSceneCount > 1)
+                _eventSystem.gameObject.SetActive(false);
+        }
+        
+        private void OnDestroy()
+        {
+            if (this == Instance)
+                Instance = null;
         }
     }
 }
