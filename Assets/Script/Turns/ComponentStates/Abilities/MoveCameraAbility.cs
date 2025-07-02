12b251402ecb2c4d336951481e2ae8c6b62e8b1e
Nyukiland <roman211113@gmail.com>
2025-07-02 16:56:57 +0200

    [UPD] CamMove - in progress

--- Assets/Script/Turns/ComponentStates/Abilities/MoveCameraAbility.cs
+++ Assets/Script/Turns/ComponentStates/Abilities/MoveCameraAbility.cs
@@ -1,4 +1,5 @@
 using CardGame.StateMachine;
+using CardGame.UI;
 using DG.Tweening;
 using UnityEngine;
 
@@ -9,16 +10,25 @@ namespace CardGame.Turns
 		[SerializeField]
 		private float _moveFactor;
 
+		[SerializeField, Min(0)]
+		private float _limitCamMove;
+
 		private Vector2 _startPos;
 		private Vector3 _camPos;
 		private Camera _cam;
 
+		private Vector3 _topRightCorner;
+		private Vector3 _bottomLeftCorner;
+
 		private bool _inUse;
+
+		private GridManagerResource _gridManager;
 
 		public override void LateInit()
 		{
 			base.LateInit();
 			_cam = Camera.main;
+			_gridManager = Owner.GetStateComponent<GridManagerResource>();
 		}
 
 		public override void OnDisable()
@@ -32,13 +42,39 @@ namespace CardGame.Turns
 			_inUse = true;
 			_startPos = pos;
 			_camPos = _cam.transform.position;
+
+			for (int i = 0; i < _gridManager.Width; i++)
+			{
+				for (int j = 0; j < _gridManager.Height; j++)
+				{
+					TileVisu tile = _gridManager.GetTile(i, j);
+
+					if (tile.TileData == null) continue;
+
+					Vector3 tilePos = tile.transform.position;
+
+					_bottomLeftCorner.x = tilePos.x < _bottomLeftCorner.x ? tilePos.x : _bottomLeftCorner.x;
+					_bottomLeftCorner.y = tilePos.y < _bottomLeftCorner.y ? tilePos.y : _bottomLeftCorner.y;
+
+					_bottomLeftCorner.x = tilePos.x > _topRightCorner.x ? tilePos.x : _topRightCorner.x;
+					_bottomLeftCorner.y = tilePos.y > _topRightCorner.y ? tilePos.y : _topRightCorner.y;
+				}
+			}
+
+			_bottomLeftCorner += Vector3.one;
+			_topRightCorner -= Vector3.one;
 		}
 
 		public void MoveCamera(Vector2 pos)
 		{
 			if (!_inUse) return;
 
-			Vector3 move = _startPos - pos;
+			Vector3 move = _cam.ScreenToWorldPoint(_startPos) - _cam.ScreenToWorldPoint(pos);
+
+			Vector2 viewport = _cam.WorldToViewportPoint(_bottomLeftCorner + move);
+			if (viewport.x > 1 || viewport.x < 0 ||
+				viewport.y > 1 || viewport.x < 0)
+				return;
 
 			_cam.transform.DOMove(_camPos + (move * _moveFactor), 0.1f);
 		}
