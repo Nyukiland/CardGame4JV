4c7c23d17eaf1b22e506507e49434d9b71c7c6d8
Nyukiland <roman211113@gmail.com>
2025-08-22 15:26:57 +0200

    [ADD] ScoringFeedback - shader + render pass

---
+++ Assets/Script/Scoring/FeedbackStuff/MeshOverlapAlphaFeature.cs
@@ -0,0 +1,114 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.RenderGraphModule;
+using UnityEngine.Rendering.Universal;
+using System.Collections.Generic;
+
+public class MeshOverlapAlphaFeature : ScriptableRendererFeature
+{
+	[System.Serializable]
+	public class Settings
+	{
+		public List<MeshRenderer> TargetMeshes = new();
+		public Shader AccumulationShader;
+		public string TextureName = "_OverlapCountTex";
+	}
+
+	public Settings settings = new();
+
+	class AccumulationPass : ScriptableRenderPass
+	{
+		private Settings _settings;
+		private Material _accumulationMat;
+		private List<MeshRenderer> _meshes = new();
+
+		public AccumulationPass(Settings settings)
+		{
+			_settings = settings;
+			if (settings.AccumulationShader != null)
+				_accumulationMat = CoreUtils.CreateEngineMaterial(settings.AccumulationShader);
+		}
+
+		public void SetMeshes(List<MeshRenderer> list)
+		{
+			_meshes = list;
+		}
+
+		public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
+		{
+			UniversalCameraData cameraData = frameData.Get<UniversalCameraData>();
+			RenderTextureDescriptor desc = cameraData.cameraTargetDescriptor;
+
+			desc.depthBufferBits = 0;
+			desc.msaaSamples = 1;
+			desc.graphicsFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.R8_UNorm; // single-channel
+
+			TextureHandle accumulationTex = renderGraph.CreateTexture(
+				new TextureDesc(desc.width, desc.height)
+				{
+					colorFormat = desc.graphicsFormat,
+					depthBufferBits = DepthBits.None,
+					name = _settings.TextureName,
+					clearBuffer = true,
+					clearColor = Color.clear
+				});
+
+			using (IRasterRenderGraphBuilder builder = renderGraph.AddRasterRenderPass<PassData>(
+				"Mesh Overlap Accumulation", out PassData passData))
+			{
+				builder.SetRenderAttachment(accumulationTex, 0);
+				builder.AllowPassCulling(false);
+
+				builder.SetRenderFunc((PassData data, RasterGraphContext ctx) =>
+				{
+					if (_accumulationMat == null || _meshes == null || _meshes.Count == 0)
+						return;
+
+					foreach (MeshRenderer meshRenderer in _meshes)
+					{
+						if (meshRenderer == null) continue;
+						MeshFilter mf = meshRenderer.GetComponent<MeshFilter>();
+						if (mf != null && mf.sharedMesh != null)
+						{
+							ctx.cmd.DrawMesh(
+								mf.sharedMesh,
+								meshRenderer.transform.localToWorldMatrix,
+								_accumulationMat);
+						}
+					}
+				});
+			}
+
+			using (IRasterRenderGraphBuilder builder = renderGraph.AddRasterRenderPass<PassData>(
+				"Expose OverlapTex", out PassData passData))
+			{
+				builder.UseTexture(accumulationTex);
+				builder.AllowPassCulling(false);
+				builder.AllowGlobalStateModification(true);
+
+				builder.SetRenderFunc((PassData data, RasterGraphContext ctx) =>
+				{
+					ctx.cmd.SetGlobalTexture(_settings.TextureName, accumulationTex);
+				});
+			}
+		}
+
+		private class PassData { }
+	}
+
+	private AccumulationPass _accumulationPass;
+
+	public override void Create()
+	{
+		_accumulationPass = new(settings)
+		{
+			renderPassEvent = RenderPassEvent.BeforeRenderingTransparents
+		};
+	}
+
+	public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+	{
+		_accumulationPass.SetMeshes(settings.TargetMeshes);
+		renderer.EnqueuePass(_accumulationPass);
+	}
+}
---
+++ Assets/Script/Scoring/FeedbackStuff/OverlapAccumulation.shader
@@ -0,0 +1,41 @@
+Shader "Hidden/OverlapAccumulation"
+{
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" "Queue"="Geometry" }
+        Pass
+        {
+            ZWrite Off
+            ZTest LEqual
+            Blend One One // additive
+
+            HLSLPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+
+            struct Attributes
+            {
+                float4 positionOS : POSITION;
+            };
+
+            struct Varyings
+            {
+                float4 positionHCS : SV_POSITION;
+            };
+
+            Varyings vert (Attributes v)
+            {
+                Varyings o;
+                o.positionHCS = TransformObjectToHClip(v.positionOS.xyz);
+                return o;
+            }
+
+            half frag() : SV_Target
+            {
+                return 1.0; // each fragment adds 1
+            }
+            ENDHLSL
+        }
+    }
+}
